5a0ce1bbac8bf6c31ff7e11570ee5c2354fe4736
abeho17 <abeho17@outlook.com>
2017-06-02 23:18:37 +0530

    activity for recovering password

--- app/src/main/java/com/skilledhacker/developer/musiqx/Database/DatabaseUpdater.java
+++ app/src/main/java/com/skilledhacker/developer/musiqx/Database/DatabaseUpdater.java
@@ -127,22 +127,4 @@ public class DatabaseUpdater {
 
     }
 
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            InputStream in = urlConnection.getInputStream();
-
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
-
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
-            }
-        } finally {
-            urlConnection.disconnect();
-        }
-    }
 }
